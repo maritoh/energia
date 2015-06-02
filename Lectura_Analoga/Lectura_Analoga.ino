eadd147912660c2d2d5d12ce249c7c0117aab823
maritoh <marito.h@gmail.com>
2015-06-02 16:01:32 -0600

    Lectura analoga

--- Lectura_Analoga/Lectura_Analoga.ino
+++ Lectura_Analoga/Lectura_Analoga.ino
@@ -11,3 +11,11 @@ void loop()
   // put your main code here, to run repeatedly:
   x++;
 }
+
+void analog_read(){
+  // read the input on analog pin A3:
+  int sensorValue = analogRead(A3);
+  // print out the value you read:
+  Serial.println(sensorValue);
+  delay(1); // delay in between reads for stability 
+}
