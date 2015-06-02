779f87766d357809d5b7898b1aa50ee51b3bb26d
marioh <marito.h@gmail.com>
2015-06-02 16:06:04 -0600

    Inicialización de LED rojo.

--- Lectura_Analoga/Lectura_Analoga.ino
+++ Lectura_Analoga/Lectura_Analoga.ino
@@ -5,6 +5,8 @@ void setup()
   x = 0;
    // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
+   // initialize the digital pin as an output.
+  pinMode(RED_LED, OUTPUT);     
 }
 
 void loop()
